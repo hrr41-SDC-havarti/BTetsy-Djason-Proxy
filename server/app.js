017886213e67e4e1fe60d0f698a743032af2e580
ingdjason <djason_sylvaince@yahoo.fr>
2019-11-14 19:00:15 -0500

    Step 1

--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
+const newrelic = require('newrelic');
 const express = require('express');
+const cors = require('cors');
 const proxy = require('http-proxy-middleware');
 const app = express();
 const PORT = 5001;
 
-app.use(proxy('/api/seller', { target: 'http://localhost:5000' }));
-app.use(proxy('/api/checkout', { target: 'http://localhost:1234' }));
-app.use(proxy('/products', { target: 'http://localhost:3000' }));
+app.use(cors());
 
+// app.use(proxy('/api/seller', { target: 'http://localhost:5000' }));
+// app.use(proxy('/api/checkout', { target: 'http://localhost:1234' }));
+// app.use(proxy('/products', { target: 'http://localhost:3000' }));
 
 app.use(express.static('./public'));
-
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
 })
-
-
-
-
